8edf94fd1986e1a08592daec3197c71fb5822bed
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-30 08:49:50 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -17,7 +17,7 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = StartStateSelector; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 144; // How many frames per second the game should run at.
+	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 	public static var currentPackGlobal:String = "";
@@ -35,6 +35,8 @@ class Main extends Sprite
 
 	public function new()
 	{
+                SUtil.uncaughtErrorHandler();
+
 		super();
 
 		if (stage != null)
@@ -73,13 +75,12 @@ class Main extends Sprite
 		}
 
 		initialState = StartStateSelector;
+                //SUtil.checkPermissions();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
 		fps = new FpsDisplay(10, 3, 0xFFFFFF);
 		var fpsFormat = new TextFormat("Comic Sans MS Bold", 15, 0xFFFFFF, true);
 		fps.defaultTextFormat = fpsFormat;
 		addChild(fps);
-		#end
 	}
 }
