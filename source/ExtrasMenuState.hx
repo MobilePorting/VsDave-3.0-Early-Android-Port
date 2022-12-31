a3ba14a2672d5a6fac305fa820528851189bc656
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-12-31 09:22:06 +0300

    Update ExtrasMenuState.hx

--- source/ExtrasMenuState.hx
+++ source/ExtrasMenuState.hx
@@ -139,6 +139,10 @@ class ExtrasMenuState extends MusicBeatState
 
 		changeItem();
 
+                #if mobile
+                addVirtualPad(UP_DOWN, A_B);
+                #end
+
 		super.create();
 	}
 
